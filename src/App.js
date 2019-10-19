f6cb7d32213851e6c74bc2d83be711a80e3ce01a
jlf <jlf@MacBook-Pro-4.local>
2019-10-19 09:03:41 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CLJ IS 9 ZHU
         </a>
       </header>
     </div>
